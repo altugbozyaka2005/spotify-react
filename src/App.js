398c91b68421ccc8132ec1bba5f851f127884952
altugbozyaka2005 <altugbozyaka@gmail.com>
2024-05-21 13:50:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav className='navbar'>Ja<span className='purple'>mmm</span>ing</nav>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App;
